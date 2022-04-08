74011ea657ee222c3a3852b43264250e78d82412
eryudi-m <eryudi-m@student.42sp.org.br>
2022-04-08 00:38:40 -0300

    test if makefile compile

--- Makefile
+++ Makefile
@@ -6,17 +6,19 @@
 #    By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/07 00:10:26 by eryudi-m          #+#    #+#              #
-#    Updated: 2022/04/07 00:34:45 by eryudi-m         ###   ########.fr        #
+#    Updated: 2022/04/08 00:27:26 by eryudi-m         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 
 NAME = libft.a
+
 SRC = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
 
 OBJ = $(SRC:.c=.o) #check if is really needed to not have spaces
 
 FLAGS = -Wall -Wextra -Werror
+
 CC = cc $(FLAGS) -c
 
 all: $(NAME)
@@ -25,7 +27,7 @@ $(NAME): $(OBJ)
 
 clean:
 	@/bin/rm -f $(OBJ)
-# rm as well bonus variable
+# rm as well for bonuses variable
 
 fclean: clean
 	@/bin/rm -f $(NAME)
---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 23:57:49 by eryudi-m          #+#    #+#             */
+/*   Updated: 2022/04/08 00:08:22 by eryudi-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+
+#endif
