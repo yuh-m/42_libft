5e5eb77c2d4d6c2defb3105901acac82f7617ce7
eryudi-m <eryudi-m@student.42sp.org.br>
2022-05-10 17:10:25 -0300

    add

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 00:11:20 by eryudi-m          #+#    #+#             */
-/*   Updated: 2022/05/10 00:21:17 by eryudi-m         ###   ########.fr       */
+/*   Updated: 2022/05/10 17:08:27 by eryudi-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	s_len = ft_strlen(s);
 	if (start > s_len)
-		return (ftstrdup(""));
+		return (ft_strdup(""));
 	if ((s_len - start) < len)
 		len = s_len - start;
 	result = malloc(sizeof(char) * (len + 1));
