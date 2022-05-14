7e61dcd497e3b4460206fafa12a8a438be7f43dc
yudi <yudi@mogari.com.br>
2022-05-14 21:39:43 +0200

    ft_memmove fix overlap memory

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 00:09:04 by eryudi-m          #+#    #+#             */
-/*   Updated: 2022/05/11 03:44:23 by eryudi-m         ###   ########.fr       */
+/*   Updated: 2022/05/14 21:38:31 by eryudi-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,24 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
-	i = 0;
-	if (((size_t)dest > (size_t)src) && ((size_t)dest - (size_t)src < n))
+	if (dest == NULL || src == NULL)
+		return (NULL);
+	if ((size_t)dest < (size_t)src)
 	{
-		i = 1;
-		while (n >= i)
-		{
-			((unsigned char *)dest)[n - i] = ((unsigned char *)src)[n - i];
-			i++;
-		}
-	}
-	else if ((src > dest) || ((size_t)src - (size_t)dest < n))
-	{
-		while (i < n)
+		i = 0;
+		while (n > i)
 		{
 			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 			i++;
+		}
+	}
+	else
+	{
+		i = n;
+		while (i > 0)
+		{
+			((unsigned char *)dest)[i - 1] = ((unsigned char *)src)[i - 1];
+			i--;
 		}
 	}
 	return (dest);
